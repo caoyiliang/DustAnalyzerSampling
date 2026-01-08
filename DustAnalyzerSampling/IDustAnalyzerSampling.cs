aaf1f9b8ebc517c902d981ffc2208244f2ce0ff9
曹一梁 <caoyiliang@me.com>
2026-01-08 20:21:27 +0800

    添加项目文件。

---
+++ DustAnalyzerSampling/IDustAnalyzerSampling.cs
@@ -0,0 +1,9 @@
+﻿using ProtocolInterface;
+
+namespace DustAnalyzerSampling
+{
+    public interface IDustAnalyzerSampling : IProtocol
+    {
+        Task<Dictionary<string, string>?> Read(string addr, int tryCount = 0, CancellationToken cancelToken = default);
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DustAnalyzerSampling;
+
+Console.WriteLine("Hello, World!");
+
+IDustAnalyzerSampling dustAnalyzerSampling = new DustAnalyzerSampling.DustAnalyzerSampling(new Communication.Bus.PhysicalPort.SerialPort("COM3", 38400));
+await dustAnalyzerSampling.OpenAsync();
+var rs = await dustAnalyzerSampling.Read("01");
+
+Console.ReadLine();
